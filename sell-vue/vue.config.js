bbc412df2bb8c57ffc6f13ebfe0183258ec0ed1a
zhongyi <zhongyi@giec.com>
2018-12-26 19:53:37 +0800

    mock data.json 模拟数据

--- sell-vue/vue.config.js
+++ sell-vue/vue.config.js
@@ -1,3 +1,8 @@
+const appData = require('./data.json')
+const seller = appData.seller
+const goods = appData.goods
+const ratings = appData.ratings
+
 module.exports = {
   css: {
     loaderOptions: {
@@ -13,6 +18,28 @@ module.exports = {
     'cube-ui': {
       postCompile: true,
       theme: true
+    }
+  },
+  devServer: {
+    before(app) {
+      app.get('/api/seller', function (req, res) {
+        res.json({
+          errno: 0,
+          data: seller
+        })
+      })
+      app.get('/api/goods', function (req, res) {
+        res.json({
+          errno: 0,
+          data: goods
+        })
+      })
+      app.get('/api/ratings', function (req, res) {
+        res.json({
+          errno: 0,
+          data: ratings
+        })
+      })
     }
   }
 }
