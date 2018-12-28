624f6a3da5c400f75765c110aa145b5947f0ee40
zhongyi <zhongyi@giec.com>
2018-12-28 19:29:48 +0800

    header-detail

--- sell-vue/src/App.vue
+++ sell-vue/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
     <v-header :seller='seller'></v-header>
+    <header-detail :seller='seller'></header-detail>
   </div>
 </template>
 
 <script>
 import VHeader from './components/v-header/v-header'
+import HeaderDetail from './components/header-detail/header-detail'
 import { getSeller } from 'api'
 
 export default {
@@ -26,7 +28,8 @@ export default {
   	}
   },
   components: {
-    VHeader
+    VHeader,
+    HeaderDetail
   }
 }
 </script>
