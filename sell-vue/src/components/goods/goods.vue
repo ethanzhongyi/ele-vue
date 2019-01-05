585b4f512792184360441283ab39c13434e69b5c
zhongyi <zhongyi@giec.com>
2019-01-05 12:30:05 +0800

    goods

--- sell-vue/src/components/goods/goods.vue
+++ sell-vue/src/components/goods/goods.vue
@@ -1,12 +1,257 @@
 <template>
   <div>
+    <div class="goods">
+      <div class="menu-wrapper" ref="menuWrapper">
+        <ul>
+          <li v-for="(item, index) in goods" :key="index" class="menu-item border-1px" :class="{'current':currentIndex === index}" @click="selectMenu(index, $event)">
+            <span class="text">
+              <Icon v-show="item.type > 0" :size="icon_3" :index="item.type"></Icon>{{item.name}}
+            </span>
+          </li>
+        </ul>
+      </div>
+      <div class="goods-wrapper" ref="goodsWrapper">
+        <ul>
+          <li v-for="item in goods" :key="item.name" ref="foodList">
+            <h1 class="title">{{item.name}}</h1>
+            <ul>
+              <li class="food-item" @click="selectFood(food, $event)" v-for="food in item.foods" :key="food.name">
+                <div class="icon">
+                  <img width="57px" height="57px" :src="food.icon">
+                </div>
+                <div class="content">
+                  <h2 class="name">{{food.name}}</h2>
+                  <p class="desc">{{food.description}}</p>
+                  <div class="extra">
+                    <span class="count">月售{{food.sellCount}}份</span>
+                    <span>好评率{{food.rating}}%</span>
+                  </div>
+                  <div class="price">
+                    <span class="now">￥{{food.price}}</span>
+                    <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                  </div>
+                  <div class="cartcontrol-wrapper">
+                    <cartcontrol @add="addFood" :food="food"></cartcontrol>
+                  </div>
+                </div>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+      <shopcart ref="shopcart" :selectFoods="selectFoods" :deliveryPrice="seller.deliveryPrice" :minPrice="seller.minPrice"></shopcart>
+    </div>
   </div>
 </template>
 
 <script>
-export default {}
+import BScroll from 'better-scroll'
+import Icon from 'components/icon-span/icon-span'
+import shopcart from 'components/shopcart/shopcart'
+import cartcontrol from 'components/cartcontrol/cartcontrol'
+
+const ERR_OK = 0
+const debug = process.env.NODE_ENV !== 'production'
+
+export default {
+  props: {
+    seller: {
+      type: Object
+    }
+  },
+  data() {
+    return {
+      goods: [],
+      icon_3: 3,
+      listHeight: [],
+      scrollY: 0,
+      selectedFood: {}
+    }
+  },
+  created() {
+    const url = debug ? '/api/goods' : 'http://ustbhuangyi.com/sell/api/goods'
+    this.$http.get(url).then((response) => {
+      response = response.data
+      if (response.errno === ERR_OK) {
+        this.goods = response.data
+        this.$nextTick(() => {
+          this._initScroll()
+          this._calcHeight()
+        })
+      }
+    })
+  },
+  computed: {
+    currentIndex() {
+      for (let i = 0; i < this.listHeight.length; i++) {
+        let height1 = this.listHeight[i]
+        let height2 = this.listHeight[i + 1]
+        if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+          return i
+        }
+      }
+      return 0
+    },
+    selectFoods() {
+      let foods = []
+      this.goods.forEach((good) => {
+        good.foods.forEach((food) => {
+          if (food.count) {
+            foods.push(food)
+          }
+        })
+      })
+      return foods
+    }
+  },
+  methods: {
+    selectMenu(index, event) {
+      // dom原生事件没用_constructed属性，如果是原生事件则退出
+      // 阻止原生click事件的触发
+      if (!event._constructed) {
+        return
+      }
+      let foodList = this.$refs.foodList
+      let el = foodList[index]
+      this.goodsScroll.scrollToElement(el, 300)
+    },
+    addFood(target) {
+      this._drop(target)
+    },
+    selectFood(food, event) {
+      if (!event._constructed) {
+        return
+      }
+      this.selectedFood = food
+      this.$refs.food.show()
+    },
+    _drop(target) {
+      // 体验优化，异步执行下落动画
+      this.$nextTick(() => {
+        this.$refs.shopcart.drop(target)
+      })
+    },
+    _calcHeight() {
+      let foodList = this.$refs.foodList
+      let height = 0
+      this.listHeight.push(height)
+      for (let i = 0; i < foodList.length; i++) {
+        let item = foodList[i]
+        height += item.clientHeight
+        this.listHeight.push(height)
+      }
+    },
+    _initScroll() {
+      this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+        click: true
+      })
+      this.goodsScroll = new BScroll(this.$refs.goodsWrapper, {
+        click: true,
+        probeType: 3
+      })
+      this.goodsScroll.on('scroll', (pos) => {
+        // 判断滑动方向，避免下拉时分类高亮错误（如第一分类商品数量为1时，下拉使得第二分类高亮）
+        if (pos.y <= 0) {
+          this.scrollY = Math.abs(Math.round(pos.y))
+        }
+      })
+    }
+  },
+  components: {
+    Icon,
+    shopcart,
+    cartcontrol
+  }
+}
 </script>
 
 <style lang="stylus" scoped>
 @import "../../common/stylus/mixin.styl"
+@import "../../common/stylus/icon.styl"
+
+.goods
+  display flex
+  position absolute
+  top 174px
+  bottom 46px
+  width 100%
+  overflow hidden
+  .menu-wrapper
+    flex 0 0 80px
+    width 80px
+    background #f3f5f7
+    .menu-item
+      display table
+      padding  0 12px
+      height 54px
+      width 56px
+      line-height 14px
+      &.current
+        position relative
+        z-index 10
+        margin-top -1px
+        background #fff
+        font-weight 700
+        .text
+          border-none()
+      .text
+        display table-cell
+        width 56px
+        vertical-align middle
+        border-1px(rgba(7, 17, 27, 0.1))
+        font-size 12px
+  .goods-wrapper
+    flex 1
+    .title
+      padding-left 14px
+      height 26px
+      line-height 26px
+      border-left 2px solid #d9dde1
+      font-size 12px
+      color rgb(147, 153, 159)
+      background #f3f5f7
+    .food-item
+      display flex
+      margin 18px
+      padding-bottom 18px
+      border-1px(rgba(7, 17, 27, 0.1))
+      &:last-child
+        border-none()
+        margin-bottom 0
+      .icon
+        flex 0 0 57px
+        margin-right 10px
+      .content
+        flex: 1
+        .name
+          margin: 2px 0 8px 0
+          height: 14px
+          line-height: 14px
+          font-size: 14px
+          color: rgb(7, 17, 27)
+        .desc, .extra
+          line-height: 10px
+          font-size: 10px
+          color: rgb(147, 153, 159)
+        .desc
+          line-height: 12px
+          margin-bottom: 8px
+        .extra
+          .count
+            margin-right: 12px
+        .price
+          font-weight: 700
+          line-height: 24px
+          .now
+            margin-right: 8px
+            font-size: 14px
+            color: rgb(240, 20, 20)
+          .old
+            text-decoration: line-through
+            font-size: 10px
+            color: rgb(147, 153, 159)
+        .cartcontrol-wrapper
+          position absolute
+          right 0
+          bottom 12px
 </style>
---
+++ sell-vue/src/components/icon-span/icon-span.vue
@@ -0,0 +1,96 @@
+<template>
+  <span class="icon" :class="[iconSize, iconIndex]"></span>
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: Number
+    },
+    index: {
+      type: Number
+    }
+  },
+  created() {
+    this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+  },
+  computed: {
+    iconSize() {
+      return 'icon_' + this.size
+    },
+    iconIndex() {
+      return this.classMap[this.index]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+@import '../../common/stylus/mixin.styl'
+
+.icon
+  display inline-block
+  vertical-align top
+  background-repeat no-repeat
+  &.icon_1
+    width 12px
+    height 12px
+    margin-right 4px
+    background-size 12px 12px
+    &.decrease
+      bg-image('decrease_1')
+    &.discount
+      bg-image('discount_1')
+    &.guarantee
+      bg-image('guarantee_1')
+    &.invoice
+      bg-image('invoice_1')
+    &.special
+      bg-image('special_1')
+  &.icon_2
+    width: 16px
+    height: 16px
+    margin-right: 6px
+    background-size: 16px 16px
+    &.decrease
+      bg-image('decrease_2')
+    &.discount
+      bg-image('discount_2')
+    &.guarantee
+      bg-image('guarantee_2')
+    &.invoice
+      bg-image('invoice_2')
+    &.special
+      bg-image('special_2')
+  &.icon_3
+    width 12px
+    height 12px
+    margin-right 2px
+    background-size 12px 12px
+    &.decrease
+      bg-image('decrease_3')
+    &.discount
+      bg-image('discount_3')
+    &.guarantee
+      bg-image('guarantee_3')
+    &.invoice
+      bg-image('invoice_3')
+    &.special
+      bg-image('special_3')
+  &.icon_4
+    width 16px
+    height 16px
+    margin-right 6px
+    background-size 16px 16px
+    &.decrease
+      bg-image('decrease_4')
+    &.discount
+      bg-image('discount_4')
+    &.guarantee
+      bg-image('guarantee_4')
+    &.invoice
+      bg-image('invoice_4')
+    &.special
+      bg-image('special_4')
+</style>
