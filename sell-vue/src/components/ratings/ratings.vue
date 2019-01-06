a899a0bd4b28276a89240bc6e5cf5f27ac236377
zhongyi <zhongyi@giec.com>
2019-01-06 16:43:58 +0800

    评价页完成

--- sell-vue/src/components/ratings/ratings.vue
+++ sell-vue/src/components/ratings/ratings.vue
@@ -10,12 +10,16 @@
         <div class="overview-right">
           <div class="score-wrapper">
             <span class="title">服务态度</span>
-            <star :size="36" :score="seller.serviceScore"></star>
+            <div class="star-wrapper">
+              <star :size="36" :score="seller.serviceScore"></star>
+            </div>
             <span class="score">{{seller.serviceScore}}</span>
           </div>
           <div class="score-wrapper">
             <span class="title">商品评分</span>
-            <star :size="36" :score="seller.serviceScore"></star>
+            <div class="star-wrapper">
+              <star :size="36" :score="seller.foodScore"></star>
+            </div>
             <span class="score">{{seller.foodScore}}</span>
           </div>
           <div class="delivery-wrapper">
@@ -24,6 +28,30 @@
           </div>
         </div>
       </div>
+      <split></split>
+      <ratingselect @select="selectRating" @toggle="toggleContent" :selectType="selectType" :onlyContent="onlyContent" :ratings="ratings"></ratingselect>
+      <div class="rating-wrapper">
+        <ul>
+          <li v-for="(rating, index) in ratings" :key="index" v-show="needShow(rating.rateType, rating.text)" class="rating-item">
+            <div class="avatar">
+              <img width="28" height="28" :src="rating.avatar">
+            </div>
+            <div class="content">
+              <h1 class="name">{{rating.username}}</h1>
+              <div class="star-wrapper">
+                <star :size="24" :score="rating.score"></star>
+                <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}分钟送达</span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommend" v-show="rating.recommend && rating.recommend.length">
+                <span class="icon-arrow_lift"></span>
+                <span class="item" v-for="(item, index) in rating.recommend" :key="index">{{item}}</span>
+              </div>
+              <div class="time">{{rating.rateTime | formatDate}}</div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -31,11 +59,12 @@
 <script>
 import BScroll from 'better-scroll'
 import {formatDate} from 'common/js/date'
-import Star from 'components/star/star'
-
+import star from 'components/star/star'
+import ratingselect from 'components/ratingselect/ratingselect'
+import split from 'components/split/split'
 const ALL = 2
 const ERR_OK = 0
-
+const debug = process.env.NODE_ENV !== 'production'
 export default {
   props: {
     seller: {
@@ -49,15 +78,60 @@ export default {
       onlyContent: true
     }
   },
+  created() {
+    const url = debug ? '/api/ratings' : 'http://ustbhuangyi.com/sell/api/ratings'
+    this.$http.get(url).then((response) => {
+      response = response.data
+      if (response.errno === ERR_OK) {
+        this.ratings = response.data
+        this.$nextTick(() => {
+          this.scroll = new BScroll(this.$refs.ratings, {
+            click: true
+          })
+        })
+      }
+    })
+  },
+  methods: {
+    needShow(type, text) {
+      if (this.onlyContent && !text) {
+        return false
+      }
+      if (this.selectType === ALL) {
+        return true
+      } else {
+        return type === this.selectType
+      }
+    },
+    selectRating(type) {
+      this.selectType = type
+      this.$nextTick(() => {
+        this.scroll.refresh()
+      })
+    },
+    toggleContent() {
+      this.onlyContent = !this.onlyContent
+      this.$nextTick(() => {
+        this.scroll.refresh()
+      })
+    }
+  },
+  filters: {
+    formatDate(time) {
+      let date = new Date(time)
+      return formatDate(date, 'yyyy-MM-dd hh:mm')
+    }
+  },
   components: {
-    Star
+    star,
+    split,
+    ratingselect
   }
 }
 </script>
 
 <style lang="stylus" scoped>
 @import "../../common/stylus/mixin.styl"
-
 .ratings
   position: absolute
   top: 174px
@@ -105,7 +179,7 @@ export default {
           vertical-align: top
           font-size: 12px
           color: rgb(7, 17, 27)
-        .star
+        .star-wrapper
           display: inline-block
           margin: 0 12px
           vertical-align: top
@@ -124,5 +198,65 @@ export default {
         .delivery
           margin-left: 12px
           font-size: 12px
+          color: rgb(147, 153, 159)
+  .rating-wrapper
+    padding: 0 18px
+    .rating-item
+      display: flex
+      padding: 18px 0
+      border-1px(rgba(7, 17, 27, 0.1))
+      .avatar
+        flex: 0 0 28px
+        width: 28px
+        margin-right: 12px
+        img
+          border-radius: 50%
+      .content
+        position: relative
+        flex: 1
+        .name
+          margin-bottom: 4px
+          line-height: 12px
+          font-size: 10px
+          color: rgb(7, 17, 27)
+        .star-wrapper
+          margin-bottom: 6px
+          font-size: 0
+          .star
+            display: inline-block
+            margin-right: 6px
+            vertical-align: top
+          .delivery
+            display: inline-block
+            vertical-align: top
+            line-height: 12px
+            font-size: 10px
+            color: rgb(147, 153, 159)
+        .text
+          margin-bottom: 8px
+          line-height: 18px
+          color: rgb(7, 17, 27)
+          font-size: 12px
+        .recommend
+          line-height: 16px
+          font-size: 0
+          .icon-arrow_lift, .item
+            display: inline-block
+            margin: 0 8px 4px 0
+            font-size: 9px
+          .icon-arrow_lift
+            color: rgb(0, 160, 220)
+          .item
+            padding: 0 6px
+            border: 1px solid rgba(7, 17, 27, 0.1)
+            border-radius: 1px
+            color: rgb(147, 153, 159)
+            background: #fff
+        .time
+          position: absolute
+          top: 0
+          right: 0
+          line-height: 12px
+          font-size: 10px
           color: rgb(147, 153, 159)
 </style>
