35d89659d879996fe706ddc5cc5598df6128f9f1
zhongyi <zhongyi@giec.com>
2019-01-06 10:54:52 +0800

    ratingselect组件

--- sell-vue/src/components/food/food.vue
+++ sell-vue/src/components/food/food.vue
@@ -18,13 +18,23 @@
             <span class="now">￥{{food.price}}</span>
             <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
           </div>
-        </div>
-        <div class="cartcontrol-wrapper">
+          <div class="cartcontrol-wrapper">
           <cartcontrol @add="addFood" :food="food"></cartcontrol>
+          </div>
+          <transition name="fade">
+            <div class="buy" @click="addFirst" v-show="!food.count || food.count===0">加入购物车</div>
+          </transition>
         </div>
-        <transition name="fade">
-          <div class="buy" @click="addFirst" v-show="!food.count || food.count===0">加入购物车</div>
-        </transition>
+        <split v-show="food.info"></split>
+        <div class="info" v-show="food.info">
+          <h1 class="title">商品信息</h1>
+          <p class="text">{{food.info}}</p>
+        </div>
+        <split></split>
+        <div class="rating">
+          <h1 class="title">商品评价</h1>
+          <ratingselect :selectType="selectType" :onlyContent="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+        </div>
       </div>
     </div>
   </transition>
@@ -34,6 +44,10 @@
 import Vue from 'vue'
 import BScroll from 'better-scroll'
 import cartcontrol from 'components/cartcontrol/cartcontrol'
+import split from 'components/split/split'
+import ratingselect from 'components/ratingselect/ratingselect'
+
+const ALL = 2
 
 export default {
 	props: {
@@ -41,12 +55,21 @@ export default {
 	},
 	data () {
 	  return {
-	  	showFlag: false
+	  	showFlag: false,
+      selectType: 1,
+      onlyContent: true,
+      desc: {
+        all: '全部',
+        positive: '推荐',
+        negative: '吐槽'
+      }
 	  }
 	},
 	methods: {
 	  show() {
 	  	this.showFlag = true
+      this.selectType = ALL
+      this.onlyContent = true
 	  	this.$nextTick(() => {
 	      if(!this.scroll){
 	  	  this.scroll = new BScroll(this.$refs.food,{
@@ -73,7 +96,9 @@ export default {
 	  }
 	},
 	components: {
-      cartcontrol
+    cartcontrol,
+    split,
+    ratingselect
   }
 }
 </script>
@@ -144,28 +169,48 @@ export default {
         text-decoration line-through
         font-size 10px
         color rgb(147, 153, 159)
-  .cartcontrol-wrapper
-    position absolute
-    right 12px
-    bottom 12px
-  .buy
-    position absolute
-    right 18px
-    bottom 18px
-    z-index 10
-    height 24px
-    line-height 24px
-    padding 0 12px
-    box-sizing border-box
-    border-radius 12px
-    font-size 10px
-    color #fff
-    background-color rgb(0, 160, 220)
-    opacity 1
-    &.fade-enter-active, &.fade-leave-active
-      transition all 0.2s
-    &.fade-enter, &.fade-leave-to
-      opacity 0
-      z-index -1
+    .cartcontrol-wrapper
+      position absolute
+      right 12px
+      bottom 12px
+    .buy
+      position absolute
+      right 18px
+      bottom 18px
+      z-index 10
+      height 24px
+      line-height 24px
+      padding 0 12px
+      box-sizing border-box
+      border-radius 12px
+      font-size 10px
+      color #fff
+      background-color rgb(0, 160, 220)
+      opacity 1
+      &.fade-enter-active, &.fade-leave-active
+        transition all 0.2s
+      &.fade-enter, &.fade-leave-to
+        opacity 0
+        z-index -1
+  .info
+    padding 18px
+    .title
+      line-height 14px
+      margin-bottom 6px
+      font-size 14px
+      color rgb(7, 17, 27)
+      font-weight 500
+    .text
+      line-height 24px
+      padding 0 8px
+      font-size 12px
+      color rgb(77, 85, 93)
+  .rating
+    padding-top 18px
+    .title
+      line-height 14px
+      margin-left 18px
+      font-size 14px
+      color rgb(7, 17, 27)
 
 </style>
